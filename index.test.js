7efbf253af5215bb8098f01c6e804da00df6de8b
rubentejera <ruben.tejera@leanmind.es>
2018-05-16 09:36:05 +0100

    Red: Padding-left App --> should be return the word add characters before the word

--- index.test.js
+++ index.test.js
@@ -7,4 +7,8 @@ describe ('Padding-left App', () => {
         test('should be return the word with send the same number of characters', function(){
             expect(paddingLeft("hola",4,"x")).toBe("hola");
         });
+        test('should be return the word add characters before the word', function(){
+            expect(paddingLeft("hola",5,"x")).toBe("xhola");
+        });
+
 });
