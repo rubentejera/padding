65d1660cb9842a7b8aba759443c6ab49233d614a
rubentejera <ruben.tejera@leanmind.es>
2018-05-16 09:32:13 +0100

    Green: Padding-left App --> should be return the word with send the same number of characters

--- index.test.js
+++ index.test.js
@@ -4,4 +4,7 @@ describe ('Padding-left App', () => {
         test('should be return the same word', function(){
             expect(paddingLeft("hola",3,"x")).toBe("hola");
         });
+        test('should be return the word with send the same number of characters', function(){
+            expect(paddingLeft("hola",4,"x")).toBe("hola");
+        });
 });
